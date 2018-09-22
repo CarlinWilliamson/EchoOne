d5b48cc01879f6c566b88fe3895b7ddd453a8d2d
CarlinWiliamson <carlin.williamson1@gmail.com>
2018-09-22 14:52:36 +1000

    clicks into course

--- src/test.py
+++ src/test.py
@@ -8,10 +8,11 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
 
-options = webdriver.FirefoxOptions()
-options.add_argument('-headless')
+#options = webdriver.FirefoxOptions()
+#options.add_argument('-headless')
 
-driver = webdriver.Firefox(firefox_options = options)
+#driver = webdriver.Firefox(firefox_options = options)
+driver = webdriver.Firefox()
 driver.get("http://www.echo360.org.au")
 #assert "Python" in driver.title
 elem = driver.find_element_by_name("email")
@@ -19,12 +20,12 @@ elem.clear()
 elem.send_keys("carlin.williamson@student.unsw.edu.au")
 elem.send_keys(Keys.RETURN)
 
-delay = 5
+delay = 4
 try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
-    print("Page is ready!")
+	myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
+	print("Page is ready!")
 except TimeoutException:
-    print("Loading took too much time!")
+	print("Loading took too much time!")
 
 elem = driver.find_element_by_id("userNameInput")
 elem.clear()
@@ -34,28 +35,42 @@ elem.clear()
 elem.send_keys("Hackathon2018")
 elem.send_keys(Keys.RETURN)
 
-delay = 10
+delay = 6
 try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
-    print("Page is ready!")
+	myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
+	print("Page is ready!")
 except TimeoutException:
-    print("Loading took too much time!")
+	print("Loading took too much time!")
+
 
 page_source = driver.page_source
 page = page_source.split('\n')
 
+
 matches = []
 for line in page:
 	matchObj = re.match( r'.*id="(\S*)".*See all classes in (\w*)', line)
+	
 	if matchObj:
 		matches.append(matchObj)
 
-
 for match in matches:
-	 #print ("matchObj.group(0) : " + match.group(0)) #Entire Statement
-	 #print ("matchObj.group(1) : " + match.group(1)) #id
-	 print ("matchObj.group(2) : " + match.group(2)) #Course Code
+	#print ("matchObj.group(0) : " + match.group(0)) #Entire Statement
+	#print ("matchObj.group(1) : " + match.group(1)) #id
+	print ("matchObj.group(2) : " + match.group(2)) #Course Code
 
+
+
+matchid = 0
+elm = driver.find_element_by_id(matches[matchid].group(1))
+elm.click()
+
+sleep(6)
+
+elms = driver.find_elements_by_class_name("menu centered")
+#for elm in elms:
+	#print(elm.get_attribute("aria-controls"))
+elms[0].click()
 
 #assert "No results found." not in driver.page_source
 #driver.close()
