89d3556bed808f1cda0486563b14798bfdb5496a
CarlinWiliamson <carlin.williamson1@gmail.com>
2018-09-22 11:47:24 +1000

    logging into Echo360

---
+++ src/test.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Firefox()
+driver.get("http://www.echo360.org.au")
+#assert "Python" in driver.title
+elem = driver.find_element_by_name("email")
+elem.clear()
+elem.send_keys("carlin.williamson@student.unsw.edu.au")
+elem.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+elem = driver.find_element_by_id("userNameInput")
+elem.clear()
+elem.send_keys("z5122521@ad.unsw.edu.au")
+elem = driver.find_element_by_name("Password")
+elem.clear()
+elem.send_keys("Hackathon2018")
+elem.send_keys(Keys.RETURN)
+
+
+
+
+#assert "No results found." not in driver.page_source
+#driver.close()
+
+
