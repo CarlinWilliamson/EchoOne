f5342bc07929f1c39cb8c56f39200d93e4380222
CarlinWiliamson <carlin.williamson1@gmail.com>
2018-09-22 17:25:34 +1000

    renames downloads

--- src/test.py
+++ src/test.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import os.path
 
 
 #options = webdriver.FirefoxOptions()
@@ -23,13 +24,7 @@ elem.clear()
 elem.send_keys("carlin.williamson@student.unsw.edu.au")
 elem.send_keys(Keys.RETURN)
 
-delay = 4
-try:
-	myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
-	print("Page is ready!")
-except TimeoutException:
-	print("Loading took too much time!")
-
+time.sleep(2)
 
 # login to microsoft
 elem = driver.find_element_by_id("userNameInput")
@@ -40,13 +35,7 @@ elem.clear()
 elem.send_keys("Hackathon2018")
 elem.send_keys(Keys.RETURN)
 
-delay = 6
-try:
-	myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
-	print("Page is ready!")
-except TimeoutException:
-	print("Loading took too much time!")
-
+time.sleep(2)
 
 # choose course
 page_source = driver.page_source
@@ -55,14 +44,8 @@ page = page_source.split('\n')
 matches = []
 for line in page:
 	matchObj = re.match( r'.*id="(\S*)".*See all classes in (\w*)', line)
-	
 	if matchObj:
 		matches.append(matchObj)
-
-for match in matches:
-	#print ("matchObj.group(0) : " + match.group(0)) #Entire Statement
-	#print ("matchObj.group(1) : " + match.group(1)) #id
-	print ("matchObj.group(2) : " + match.group(2)) #Course Code
 
 matchDict = {};
 for match in matches:
@@ -74,20 +57,14 @@ keys = list(matchDict.keys());
 for key in keys:
 	 print(key + ": " + str(counter));
 	 counter += 1
-inputNum = int(input("\nSelect a Courses Corresponding Number: "))
+courseInput = int(input("\nSelect a Courses Corresponding Number: "))
 
-matchid = inputNum
-print(matchDict[keys[inputNum]])
-elm = driver.find_element_by_id(matchDict[keys[inputNum]])
+matchid = courseInput
+print(matchDict[keys[courseInput]])
+elm = driver.find_element_by_id(matchDict[keys[courseInput]])
 elm.click()
 
-delay = 1
-try:
-	myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
-	print("Page is ready!")
-except TimeoutException:
-	print("Loading took too much time!")
-
+time.sleep(2)
 
 # choose lecture video
 elms = driver.find_elements_by_class_name("menu-opener")
@@ -97,28 +74,33 @@ for elm in elms:
 	matchObj = re.match( r'.*_(\d{4})-(\d{2})-(\d{2})T.*', elm.get_attribute("aria-controls"))
 	print(matchObj.group(3) + "/" + matchObj.group(2) + "/" + matchObj.group(1) + ": " + str(counter))
 	counter += 1
-inputNum = int(input("\nSelect a Lectures Corresponding Number: "))
+lectureInput = int(input("\nSelect a Lectures Corresponding Number: "))
 
 elms = driver.find_elements_by_class_name("courseMediaIndicator")
-elms[inputNum].click()
-time.sleep(1)
+elms[lectureInput].click()
+time.sleep(0.5)
 
 # open download page
-elm = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/div/div[2]/div[" + str(inputNum + 1) + "]/div/div/div/div/div/div[2]/ul/li[2]/a")
+elm = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/div/div[2]/div[" + str(lectureInput + 1) + "]/div/div/div/div/div/div[2]/ul/li[2]/a")
 elm.click()
-time.sleep(1)
+time.sleep(0.5)
 
 # make the video hd
-elm = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/div/div[1]/div[4]/div[1]/div/div/select/option[2]")
-elm.click()
+#elm = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/div/div[1]/div[4]/div[1]/div/div/select/option[2]")
+#elm.click()
 
 #download the video
 elm = driver.find_element_by_class_name("downloadBtn")
 elm.click()
 
+os.chdir("/home/carlin/Downloads/")
+while not os.path.exists("sd1.mp4"):
+	time.sleep(1)
+os.rename("sd1.mp4", keys[courseInput] + "-" + str(lectureInput))
+
+print("download finished and renamed to " + keys[courseInput] + "-" + str(lectureInput))
 
 #assert "No results found." not in driver.page_source
 #driver.close()
 
 # vim: set softtabstop=8
-
